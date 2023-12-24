0284b019f8628affef1461a9dbbc08edb35794b8
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2023-12-24 22:54:14 +0200

    update

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:3000", // Replace with your frontend domain
+  origin: "https://portifolio-77rh.onrender.com", // Replace with your frontend domain
   optionsSuccessStatus: 200, // Some legacy browsers choke on 204
 };
 // Middleware Setup
