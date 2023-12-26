314559d0f756d54c34c314cf2b84a34d0fb6697d
Abanob Shenoda <121344723+pepoo202020@users.noreply.github.com>
2023-12-26 14:54:58 +0200

    update

--- server.js
+++ server.js
@@ -17,8 +17,9 @@ const corsOptions = {
 };
 // Middleware Setup
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-app.use(express.json());
+
 app.use(cors(corsOptions)); // Enable Cors
 app.use(bodyParser.json()); // Parse Json requests
 app.use(cookieParser());
